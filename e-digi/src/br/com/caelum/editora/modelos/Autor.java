8a25a07d309c6c9e9580795bb87338cb01f0ea7c
Felipe <martinsfelipe62@gmail.com>
2020-05-19 20:19:18 -0300

    Retirando métodos inutilizados

--- e-digi/src/br/com/caelum/editora/modelos/Autor.java
+++ e-digi/src/br/com/caelum/editora/modelos/Autor.java
@@ -16,16 +16,8 @@ public class Autor {
 		return nome;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
 	public String getEmail() {
 		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
 	}
 
 	private void verificaSeTemTexto(String input, String message) {
