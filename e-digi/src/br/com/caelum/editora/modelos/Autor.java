8a13f339663bc47afe4184eeab6d6c5046f0d739
Felipe <martinsfelipe62@gmail.com>
2020-05-20 15:23:07 -0300

    Retirando mais métodos inutilzados

--- e-digi/src/br/com/caelum/editora/modelos/Autor.java
+++ e-digi/src/br/com/caelum/editora/modelos/Autor.java
@@ -34,13 +34,7 @@ public class Autor {
 		}
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		return result;
-	}
+	
 
 	@Override
 	public boolean equals(Object obj) {
